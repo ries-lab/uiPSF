92268018a1d9893b654cbcb9c400f0fa0dd4e6be
kiwibogo <kiwibogo@gmail.com>
2023-05-01 09:48:48 -0600

    fix bug from insitu psf

--- psflearning/learning/fitters/Fitter_file.py
+++ psflearning/learning/fitters/Fitter_file.py
@@ -161,7 +161,7 @@ class Fitter(FitterInterface):
             intensity = np.min(intensity,axis=0,keepdims=False)
             mse1 = np.sum(mse1,axis=0,keepdims=False)
         a = threshold[0]
-        if self.psf.options.backgroundROI:
+        if self.psf.options.insitu.backgroundROI:
             mask = (xp>np.quantile(xp,1-a)) & (xp<np.quantile(xp,a)) & (yp>np.quantile(yp,1-a)) & (yp<np.quantile(yp,a)) 
         else:
             mask = (xp>np.quantile(xp,1-a)) & (xp<np.quantile(xp,a)) & (yp>np.quantile(yp,1-a)) & (yp<np.quantile(yp,a)) & (zp>np.quantile(zp,1-a)) & (zp<np.quantile(zp,a))
